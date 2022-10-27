18e991f03fa5f1bc7f64bd1085f1ca3b3c54d156
gwltr <68957540+gwltr@users.noreply.github.com>
2022-10-27 14:45:03 +0200

    reduce controller and refactor into button

--- button.py
+++ button.py
@@ -1,19 +1,44 @@
 from machine import Pin
-import time
+from machine import Timer
 
 class Button:
 
-    def __init__(self, pin_no, callback):
-        self.pin_no = pin_no
-        self.pressed = False
-        self.btn = Pin(self.pin_no, Pin.IN, Pin.PULL_DOWN)
-        self.btn.irq(trigger=Pin.IRQ_FALLING, handler=callback)
+    def __init__(self, pin_no):
+        self.DEBOUNCE_DURATION = 100
         
-    def is_pressed(self):
-        return self.pressed
+        self.button = Pin(pin_no, Pin.IN, Pin.PULL_DOWN)
+        self.debounce_timer = Timer()
+        self.is_debounce_active = False
+        
+        self.button.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_wrapper)
+        
+        self.callback = None
+        
+    def on_press(self, callback): # The callback NEEDS to take one parameter. For example callback(btn_pressed)
+        self.callback = callback
+        # it's not enough to just redefine callback_up,the whole ISR needs to be redefined.
+        self.button.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_wrapper)
     
+    def callback_wrapper(self, btn_pressed):
+        if self.is_debounce_active:
+            print(".", end='')
+            return
+
+        def reset_debounce(t): self.is_debounce_active = False
+        self.debounce_timer.init(mode=Timer.ONE_SHOT, period=self.DEBOUNCE_DURATION, callback=reset_debounce)
+
+        self.is_debounce_active = True
+        
+        if self.callback == None:
+            print('No callback for "' + str(btn_pressed) + '" defined.')
+            return
+
+        self.callback(btn_pressed)
+        
     def clear(self):
         self.pressed = False
 
 if __name__ == "__main__":
    btn = Button(16)
+   btn.on_press(lambda x: print ('Button press works.'))
+
--- controller.py
+++ controller.py
@@ -4,125 +4,33 @@
 '''
 
 from button import Button
-from machine import Timer
-from machine import Pin
 
 class Controller:
     def __init__(self):
         self.DEBOUNCE_DURATION = 100
 
-        self.button_up = Pin(16, Pin.IN, Pin.PULL_DOWN)
-        self.button_down = Pin(18, Pin.IN, Pin.PULL_DOWN)
-        self.button_left = Pin(17, Pin.IN, Pin.PULL_DOWN)
-        self.button_right = Pin(19, Pin.IN, Pin.PULL_DOWN)
-
-        self.debounce_up_timer = Timer()
-        self.debounce_down_timer = Timer()
-        self.debounce_left_timer = Timer()
-        self.debounce_right_timer = Timer()
+        self.button_up = Button(16)
+        self.button_down = Button(19)
+        self.button_left = Button(18)
+        self.button_right = Button(15)
         
-        self.is_debounce_up_active = False
-        self.is_debounce_down_active = False
-        self.is_debounce_left_active = False
-        self.is_debounce_right_active = False
-    
-        self.button_up.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_up_wrapper)
-        self.button_down.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_down_wrapper)
-        self.button_left.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_left_wrapper)
-        self.button_right.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_right_wrapper)
-                
-        self.callback_up = None        
-        self.callback_down = None
-        self.callback_left = None
-        self.callback_right = None
-    
-    def on_up(self, callback): # The callback NEEDS to take one parameter. For example callback(btn_pressed)
-        self.callback_up = callback
-        # it's not enough to just redefine callback_up,the whole ISR needs to be redefined.
-        self.button_up.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_up_wrapper)
+    def on_up(self, callback):
+        self.button_up.on_press(callback)
 
     def on_down(self, callback):
-        self.callback_down = callback
-        # it's not enough to just redefine callback_down,the whole ISR needs to be redefined.
-        self.button_down.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_down_wrapper)
+        self.button_down.on_press(callback)
 
     def on_left(self, callback):
-        self.callback_left = callback
-        # it's not enough to just redefine callback_left,the whole ISR needs to be redefined.
-        self.button_left.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_left_wrapper)
+        self.button_left.on_press(callback)
 
     def on_right(self, callback):
-        self.callback_right = callback
-        # it's not enough to just redefine callback_right,the whole ISR needs to be redefined.
-        self.button_right.irq(trigger=Pin.IRQ_FALLING, handler=self.callback_right_wrapper)
+        self.button_right.on_press(callback)
 
-    def callback_up_wrapper(self, btn_pressed):
-        if self.is_debounce_up_active:
-            print(".", end='')
-            return
-
-        def reset_debounce(t): self.is_debounce_up_active = False
-        self.debounce_up_timer.init(mode=Timer.ONE_SHOT, period=self.DEBOUNCE_DURATION, callback=reset_debounce)
-
-        self.is_debounce_up_active = True
-        
-        if self.callback_up == None:
-            print('No callback for "up" defined.')
-            return
-
-        self.callback_up(btn_pressed)
-
-    def callback_down_wrapper(self, btn_pressed):
-        if self.is_debounce_down_active:
-            print(".", end='')
-            return
-            
-        def reset_debounce(t): self.is_debounce_down_active = False
-        self.debounce_down_timer.init(mode=Timer.ONE_SHOT, period=self.DEBOUNCE_DURATION, callback=reset_debounce)
-
-        self.is_debounce_down_active = True
-        
-        if self.callback_down == None:
-            print('No callback for "up" defined.')
-            return
-
-        self.callback_down(btn_pressed)
-
-    def callback_left_wrapper(self, btn_pressed):
-        if self.is_debounce_left_active:
-            print(".", end='')
-            return
-            
-        def reset_debounce(t): self.is_debounce_left_active = False
-        self.debounce_left_timer.init(mode=Timer.ONE_SHOT, period=self.DEBOUNCE_DURATION, callback=reset_debounce)
-
-        self.is_debounce_left_active = True
-        
-        if self.callback_left == None:
-            print('No callback for "up" defined.')
-            return
-
-        self.callback_left(btn_pressed)
-
-    def callback_right_wrapper(self, btn_pressed):
-        if self.is_debounce_right_active:
-            print(".", end='')
-            return
-            
-        def reset_debounce(t): self.is_debounce_right_active = False
-        self.debounce_right_timer.init(mode=Timer.ONE_SHOT, period=self.DEBOUNCE_DURATION, callback=reset_debounce)
-
-        self.is_debounce_right_active = True
-        
-        if self.callback_right == None:
-            print('No callback for "up" defined.')
-            return
-
-        self.callback_right(btn_pressed)
 
 if __name__ == "__main__":
     ctrl = Controller()
-    ctrl.on_up(lambda btn: print(btn))
-    ctrl.on_down(lambda btn: print(btn))
-    ctrl.on_left(lambda btn: print(btn))
-    ctrl.on_right(lambda btn: print(btn))
+    ctrl.on_up(lambda btn: print('up'))
+    ctrl.on_down(lambda btn: print('down'))
+    ctrl.on_left(lambda btn: print('left'))
+    ctrl.on_right(lambda btn: print('right'))
+
