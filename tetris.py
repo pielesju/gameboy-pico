bfa2b16d7afbcbaa9b75cc1554183cc31a74304d
gwltr <mail@gabrielwalter.de>
2022-12-01 13:49:38 +0100

    Begin Tetris

--- tetris.py
+++ tetris.py
@@ -1,21 +1,62 @@
+import machine
+import time
+from button import Button
+from game import Game
+from machine import Timer, Pin
+from display import Display
+from controller import Controller
 
-def tetris():
-    y = 0
-    x = 0
-    blocks = []
-    while True:
-        display.fill(0)
-        display.pixel(0 + x,0 + y,1)
-        display.pixel(1 + x,0 + y,1)
-        display.pixel(2 + x,0 + y,1)
-        display.pixel(2 + x,1 + y,1)
 
-        display.show()
-        time.sleep(0.3)
-        if controller.left() and y + 3 < 7:
-            x = x - 1
-        if controller.right() and y + 3 < 7:
-            x = x + 1
-        if(y + 3 < 7):
-            y = y + 1
-# END OF tetris()
+
+class Tetris(Game):
+    def __init__(self, display, controller, menu):
+        Game.__init__(self, display, controller, menu) # pass parameters into parent class
+
+        self.lastButtonPressed = None
+
+
+    def loop(self, t):
+        self.led.toggle()
+
+    def run(self):
+        def up_fn(btn_pressed):    self.lastButtonPressed = 'up'
+        def down_fn(btn_pressed):  self.lastButtonPressed = 'down'
+        def left_fn(btn_pressed):  self.lastButtonPressed = 'left'
+        def right_fn(btn_pressed): self.lastButtonPressed = 'right'
+        def a_fn(btn_pressed):     self.lastButtonPressed = 'a'
+        def b_fn(btn_pressed):     self.exit()
+
+        self.controller.on_up(up_fn)
+        self.controller.on_down(down_fn)
+        self.controller.on_left(left_fn)
+        self.controller.on_right(right_fn)
+        self.controller.on_a(a_fn)
+        self.controller.on_b(b_fn)
+
+        self.gameLoop.init(mode=Timer.PERIODIC, period=1000, callback=self.loop)
+
+if __name__ == "__main__":
+    display = Display()
+    controller = Controller()
+    menu = Menu(display, controller)
+
+    game = Tetris(display, controller, menu)
+    game.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
